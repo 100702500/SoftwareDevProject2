b8233e59b553efc66b90aac92b1548ac9a7ea1d7
100702500 <100702500@student.swin.edu.au>
2017-09-04 23:29:15 +1000

    2/09/2017
    basic start of read item
    will read from file and return result as a record object

---
+++ Project/Project/readitem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Project
+{
+    //static
+    public class PHP_f_read
+    {
+        const char delimiter = ',';
+        public PHP_f_read()
+        {
+        }
+
+        /// <summary>
+        /// get file location of specified date
+        /// </summary>
+        public string datalocation(string date)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            path += "\\" + date + ".csv";
+            return path;
+        }
+
+        /// <summary>
+        /// returns sale record
+        /// </summary>
+        /// <param name="location"></param>
+        public List<Item> loadfile(string location)
+        {
+            List<Item> saleItems = new List<Item>();
+            
+            string line;
+            int counter = 0;
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader(location);
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] details = line.Split(delimiter);
+                Item record = new Item(details[0], float.Parse(details[1]), int.Parse(details[2]));
+                saleItems.Add(record);
+
+                Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            return saleItems;
+        }
+
+        /// <summary>
+        /// return array of items matching clause
+        /// </summary>
+        /// <param name="conditioncolumn">which column will be </param>
+        /// <param name="condition">a condition to be checked</param>
+        public void searchforitem(int conditioncolumn/*, condition*/)
+        {
+            //a = list of sales record
+            //for all files in path = c
+            //x = loadfile(c)
+            //a.add(x.search);
+        }
+
+        //get file location
+        //get file data
+        //search file for items based on condtion
+    }
+}
